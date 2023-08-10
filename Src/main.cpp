8232ae9ae2be686d61624d08e7dc0af301f68b0a
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-08-10 13:28:03 +0300

    rm not needed includes

--- Src/main.cpp
+++ Src/main.cpp
@@ -7,9 +7,8 @@
 #include <user_functions.h>
 #include <delay.h>
 #include <timer.h>
-#include <ultrasonic_sensor.h>
 #include <stdint.h>
-#include <uart.h>
+#include <i2c.h>
 
 // DEBUG
 #include <debug_functons.h>
@@ -20,19 +19,44 @@
 #endif
 
 // Pin declaration
+pin_struct_TypeDef scl_i2c_pb6;
+pin_struct_TypeDef sda_i2c_pb7;
 
 // Functions
 void clock_init()
 {
+  // Enable AHB1 for GPIO
+  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+
+  // Enable APB2 for I2C1
+  SET_BIT(RCC->APB1ENR, RCC_APB1ENR_I2C1EN);
 }
 void pin_init()
 {
+  /******I2C PINS******/
+  scl_i2c_pb6 = pin_setup(GPIOB, PIN6, ALTERNATE);
+  // Select AF
+  SET_BIT(scl_i2c_pb6.GPIOx->AFR[0], GPIO_AFRL_AFRL6_2);
+  // Select open drain
+  SET_BIT(scl_i2c_pb6.GPIOx->OTYPER, GPIO_OTYPER_OT_6);
+  // Set to pull-up mode
+  SET_BIT(scl_i2c_pb6.GPIOx->PUPDR, GPIO_PUPDR_PUPD6_0);
+
+  sda_i2c_pb7 = pin_setup(GPIOB, PIN7, ALTERNATE);
+  // Select AF
+  SET_BIT(sda_i2c_pb7.GPIOx->AFR[0], GPIO_AFRL_AFRL7_2);
+  // Select open drain
+  SET_BIT(sda_i2c_pb7.GPIOx->OTYPER, GPIO_OTYPER_OT_7);
+  // Set to pull-up mode
+  SET_BIT(sda_i2c_pb7.GPIOx->PUPDR, GPIO_PUPDR_PUPD7_0);
+  /********************/
 }
 
 int main(void)
 {
   clock_init();
   pin_init();
+  i2c1_init();
 
   /* Loop forever */
   while (true)
